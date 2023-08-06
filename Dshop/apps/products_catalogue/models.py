7e88cbdc4fc4ca332def5186f908a15f9a9c2f4e
Robert Lewandowski <rl89pl@gmail.com>
2023-08-06 20:15:02 +0200

    Added Category and SubCategory models with tests

--- Dshop/apps/products_catalogue/models.py
+++ Dshop/apps/products_catalogue/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    is_active = models.BooleanField(default=True)
+    sort = models.IntegerField(default=99)
+
+    class Meta:
+        ordering = ('sort',)
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+
+class SubCategory(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    is_active = models.BooleanField(default=True)
+    sort = models.IntegerField(default=99)
+
+    class Meta:
+        ordering = ('sort',)
+        verbose_name = 'Subcategory'
+        verbose_name_plural = 'Subcategories'
+
+    def __str__(self):
+        return self.name
+
+
--- Dshop/apps/products_catalogue/tests.py
+++ Dshop/apps/products_catalogue/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from .models import Category, SubCategory
 
-# Create your tests here.
+
+class TestCategoriesModels(TestCase):
+    def setUp(self):
+        self.category = Category.objects.create(
+            name='Category',
+            slug='category',
+            is_active=True,
+            sort=1
+        )
+        self.subcategory = SubCategory.objects.create(
+            category=self.category,
+            name='Subcategory',
+            slug='subcategory',
+            is_active=True,
+            sort=1
+        )
+
+    def test_create_category(self):
+        self.assertIsInstance(self.category, Category)
+
+    def test_create_subcategory(self):
+        self.assertIsInstance(self.subcategory, SubCategory)
