220411bfb9e9f38f6282d2ad77aed2176d6efdda
Robert Lewandowski <rl89pl@gmail.com>
2023-08-06 20:15:26 +0200

    Added admin.py

--- Dshop/apps/products_catalogue/admin.py
+++ Dshop/apps/products_catalogue/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Category, SubCategory
 
-# Register your models here.
+
+class SubCategoryInline(admin.TabularInline):
+    model = SubCategory
+    prepopulated_fields = {'slug': ('name',)}
+
+    def get_extra(self, request, obj=None, **kwargs):
+        extra = 0
+        return extra
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'is_active', 'sort')
+    list_editable = ('is_active', 'sort')
+    prepopulated_fields = {'slug': ('name',)}
+    inlines = [SubCategoryInline]
