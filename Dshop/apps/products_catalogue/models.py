57112dcddc145a3a5e65c19ff54c2e0c492f6248
Robert Lewandowski <rl89pl@gmail.com>
2023-08-06 20:21:52 +0200

    Refactoring Category and Subcategory models

--- Dshop/apps/products_catalogue/models.py
+++ Dshop/apps/products_catalogue/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Category(models.Model):
+class CategoryBaseModel(models.Model):
     name = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     is_active = models.BooleanField(default=True)
@@ -9,26 +9,24 @@ class Category(models.Model):
 
     class Meta:
         ordering = ('sort',)
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class Category(CategoryBaseModel):
+
+    class Meta:
         verbose_name = 'Category'
         verbose_name_plural = 'Categories'
 
-    def __str__(self):
-        return self.name
 
-
-class SubCategory(models.Model):
+class SubCategory(CategoryBaseModel):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    name = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(max_length=200, unique=True)
-    is_active = models.BooleanField(default=True)
-    sort = models.IntegerField(default=99)
 
     class Meta:
-        ordering = ('sort',)
         verbose_name = 'Subcategory'
         verbose_name_plural = 'Subcategories'
-
-    def __str__(self):
-        return self.name
 
 
