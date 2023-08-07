fd7d0bb11756698063688295a43c7880bb1f22ea
Robert Lewandowski <rl89pl@gmail.com>
2023-08-07 23:59:11 +0200

    Add slugify str to slug field when saving

--- Dshop/apps/products_catalogue/models.py
+++ Dshop/apps/products_catalogue/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.utils.text import slugify
 
 class CategoryBaseModel(models.Model):
     name = models.CharField(max_length=200, unique=True)
@@ -13,6 +13,11 @@ class CategoryBaseModel(models.Model):
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        return super().save(*args, **kwargs)
 
 
 class Category(CategoryBaseModel):
