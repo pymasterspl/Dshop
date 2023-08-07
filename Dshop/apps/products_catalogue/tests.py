c8ca91e2a6fd71f74d2b5cb30e602c25838d3875
Robert Lewandowski <rl89pl@gmail.com>
2023-08-08 00:22:11 +0200

    Commenting unused code - waiting for pytest tests functions

--- Dshop/apps/products_catalogue/tests.py
+++ Dshop/apps/products_catalogue/tests.py
@@ -1,25 +1,16 @@
-from django.test import TestCase
-from .models import Category, SubCategory
-
-
-class TestCategoriesModels(TestCase):
-    def setUp(self):
-        self.category = Category.objects.create(
-            name='Category',
-            slug='category',
-            is_active=True,
-            sort=1
-        )
-        self.subcategory = SubCategory.objects.create(
-            category=self.category,
-            name='Subcategory',
-            slug='subcategory',
-            is_active=True,
-            sort=1
-        )
-
-    def test_create_category(self):
-        self.assertIsInstance(self.category, Category)
-
-    def test_create_subcategory(self):
-        self.assertIsInstance(self.subcategory, SubCategory)
+# from django.test import TestCase
+# from .models import Category
+#
+#
+# class TestCategoriesModels(TestCase):
+#     def setUp(self):
+#         self.category = Category.objects.create(
+#             name='Biuro i praca',
+#             is_active=True,
+#         )
+#
+#     def test_create_category(self):
+#         self.assertIsInstance(self.category, Category)
+#
+#     def test_category_slug(self):
+#         self.assertEqual(self.category.slug, 'biuro-i-praca')
