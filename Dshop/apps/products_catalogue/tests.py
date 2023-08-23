c93a5127c4cbbb736c3276530b677c9f2c5be521
memorisanka <j.krawczyk87@gmail.com>
2023-08-23 22:39:50 +0200

    tests: add tests for generating XML file for ceneo

--- Dshop/apps/products_catalogue/tests.py
+++ Dshop/apps/products_catalogue/tests.py
@@ -1,5 +1,10 @@
 import pytest
 from apps.products_catalogue.models import Category
+from django.urls import reverse
+from lxml import etree
+from django.test import RequestFactory
+from apps.products_catalogue.models import Product
+from apps.products_catalogue.views import ProductListView
 
 
 @pytest.mark.django_db
@@ -15,3 +20,30 @@ def test_create_category_with_parent():
     parent_category = Category.objects.create(name='Parent Category', is_active=True)
     child_category = Category.objects.create(name='Child Category', is_active=True, parent=parent_category)
     assert child_category.parent == parent_category
+
+
+# tests for creating XML file for Ceneo
+@pytest.fixture
+def products():
+    return [
+        Product.objects.create(name='Telewizor SAMSUNG QE65Q77B 65" QLED 4K 120HZ Tizen TV ', price=3999.00,
+                               full_description='Description 1'),
+        Product.objects.create(name='Zegarek sportowy GARMIN Venu 2 Plus Biały', price=1739.00,
+                               full_description='Description 2'),
+    ]
+
+
+@pytest.mark.django_db
+def test_generate_xml_file_for_ceneo(products):
+    request_factory = RequestFactory()
+    request = request_factory.get(reverse('product-list'))
+
+    view = ProductListView.as_view()
+    response = view(request)
+
+    assert response.status_code == 200
+    assert response['Content-Type'] == 'application/xml'
+    assert response['Content-Disposition'] == 'attachment; filename="ceneo.xml"'
+
+    root = etree.fromstring(response.content)
+    assert root.tag == 'offers'
