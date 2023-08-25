de3780a30603a9eb6528b9c62d8eead81282f039
Marta Sommer <marta.sommer90@gmail.com>
2023-08-25 11:52:38 +0200

    adding base.html and products view + template

--- Dshop/Dshop/settings.py
+++ Dshop/Dshop/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 from decouple import config
 import json
@@ -68,7 +68,7 @@ ROOT_URLCONF = 'Dshop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- Dshop/apps/products_catalogue/views.py
+++ Dshop/apps/products_catalogue/views.py
@@ -1,3 +1,15 @@
-#from django.shortcuts import render
+from django.shortcuts import render
+from django.views import View
+from .models import Product
 
 # Create your views here.
+class ProductListView(View):
+    template_name = 'products_catalogue/products_list.html'
+    model = Product
+    def get(self, request):
+        all_products = Product.objects.all()
+
+        return render(request, self.template_name, {'all_products': all_products, })
+
+
+
