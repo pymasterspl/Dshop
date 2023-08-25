6598f545bd2136084c16dcaec0a7dfdfffe41f2e
Marta Sommer <marta.sommer90@gmail.com>
2023-08-25 16:49:13 +0200

    adding context_processors module

--- Dshop/Dshop/settings.py
+++ Dshop/Dshop/settings.py
@@ -76,6 +76,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+
+                'apps.core.context_processors.add_variable_to_context',
             ],
         },
     },
