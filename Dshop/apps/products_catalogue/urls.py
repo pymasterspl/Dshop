d193ca115acee9d182f3d14cca7042deba42dcba
Jacek Ostański <jacek@ivolution.pl>
2023-08-30 10:13:51 +0200

    Revert "couple example tests for products"
    This reverts commit 66a7db0db31483d3fdb7783b934fa9afbef565ac.

--- Dshop/apps/products_catalogue/urls.py
+++
@@ -1,7 +0,0 @@
-# urls.py
-from django.urls import path
-from .views import IndexView
-
-urlpatterns = [
-    path("/", IndexView.as_view(), name="index"),
-]
--- Dshop/apps/products_catalogue/views.py
+++ Dshop/apps/products_catalogue/views.py
@@ -1,10 +1,3 @@
 #from django.shortcuts import render
 
 # Create your views here.
-
-from django.views.generic import ListView
-from .models import Product
-
-class IndexView(ListView):
-    template_name = "index.html"
-    model = Product
