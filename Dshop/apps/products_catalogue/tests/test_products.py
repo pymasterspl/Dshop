8ed75c1aeadc74b5d6d74d3a7b0c059822a4de6b
memorisanka <j.krawczyk87@gmail.com>
2023-08-31 22:11:13 +0200

    tests: fix tests

--- Dshop/apps/products_catalogue/tests/test_products.py
+++ Dshop/apps/products_catalogue/tests/test_products.py
@@ -1,5 +1,4 @@
 import pytest
-from django.test import Client
 from django.urls import reverse
 from lxml import etree
 
@@ -21,26 +20,20 @@ def test_create_category_with_parent():
     assert child_category.parent == parent_category
 
 
-# tests for creating XML file for Ceneo
-@pytest.fixture
-def products():
-    category = Category.objects.create(name='Test Category', is_active=True)
-    return [
-        Product.objects.create(name='Telewizor SAMSUNG QE65Q77B 65" QLED 4K 120HZ Tizen TV ', price=3999.00,
-                               full_description='Description 1', category=category),
-        Product.objects.create(name='Zegarek sportowy GARMIN Venu 2 Plus Biały', price=1739.00,
-                               full_description='Description 2', category=category),
-    ]
-
-
 @pytest.mark.django_db
-def test_generate_xml_file_for_ceneo(products):
-    client = Client()
-    response = client.get(reverse('product-list'))
+def test_generate_xml_file_for_ceneo(client):
+    category = Category.objects.create(name='Test Category', is_active=True)
+
+    Product.objects.create(name='Telewizor SAMSUNG QE65Q77B 65" QLED 4K 120HZ Tizen TV ', price=3999.00,
+                           full_description='Description 1', category=category)
+    Product.objects.create(name='Zegarek sportowy GARMIN Venu 2 Plus Biały', price=1739.00,
+                           full_description='Description 2', category=category)
+
+    url = reverse('product-list')
+    response = client.get(url)
 
     assert response.status_code == 200
     assert response['Content-Type'] == 'application/xml'
-    assert response['Content-Disposition'] == 'attachment; filename="ceneo.xml"'
 
     root = etree.fromstring(response.content)
     assert root.tag == 'offers'
