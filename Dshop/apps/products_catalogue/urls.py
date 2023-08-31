67214285a8dc12c51f241051e903a4ad7e7fb96f
Marta Sommer <marta.sommer90@gmail.com>
2023-08-31 13:18:05 +0200

    comment delete

--- Dshop/apps/products_catalogue/urls.py
+++ Dshop/apps/products_catalogue/urls.py
@@ -4,8 +4,6 @@ from django.conf.urls.static import static
 from .views import ProductListView
 
 urlpatterns = [
-
-    # R
     path('products_list/', ProductListView.as_view(), name='products-list'),
 ]
 
