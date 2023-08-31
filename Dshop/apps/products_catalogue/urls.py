db46638fc4963866b0f7988e2c1443b7c6cde3f1
knop-k <knop.krzysiek@gmail.com>
2023-08-31 11:21:23 +0200

    add ProdurctDetailView in urls

--- Dshop/apps/products_catalogue/urls.py
+++ Dshop/apps/products_catalogue/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import ProductListView
+from .views import ProductListView, ProductDetailView
 
 urlpatterns = [
 
     # R
     path('products_list/', ProductListView.as_view(), name='products-list'),
+    path("products/<slug>", ProductDetailView.as_view(), name='product-detail'),
 ]
 
 if settings.DEBUG:
