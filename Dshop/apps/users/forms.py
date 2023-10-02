c91023512769ac748356e23e30a83739f186d01a
Pawelooo <paweloo0147@gmail.com>
2023-10-02 10:46:00 +0200

    Add forms to users

--- Dshop/apps/users/forms.py
+++ Dshop/apps/users/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 
-from .models import User
+from .models import User, CustomUser
 
 
 class CustomUserForm(UserCreationForm):
@@ -11,8 +11,20 @@ class CustomUserForm(UserCreationForm):
 
 
 class LoginUserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
+    password = forms.CharField(widget=forms.PasswordInput())
 
     class Meta:
         model = User
         fields = ['username', 'password']
+
+
+class UpdateCustomUserForm(forms.ModelForm):
+    class Meta:
+        model = CustomUser
+        exclude = ['user', 'date_joined']
+
+
+class UpdateUserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email']
