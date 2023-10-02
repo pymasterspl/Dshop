f07ea4219993ac8402d5fdee6338bc6bcd331da2
Pawelooo <paweloo0147@gmail.com>
2023-10-02 11:08:52 +0200

    feature/user-management

--- Dshop/apps/users/urls.py
+++ Dshop/apps/users/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 
 from . import views
+from .views import RegistrationView, LoginUserView, LogoutView, HomeView, \
+    RemoveUserView, UpdateUserView
 
-
-
+urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
+    path('registration/', RegistrationView.as_view(), name='registration'),
+    path('login/', LoginUserView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('delete-user/', RemoveUserView.as_view(), name='delete_user'),
+    path('update-user/', UpdateUserView.as_view(), name='update_user')
+]
 
