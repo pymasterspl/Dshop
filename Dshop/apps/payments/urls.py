da7ea68a12950c3340fa8d7d6f174f3f945d3d34
memorisanka <j.krawczyk87@gmail.com>
2023-10-03 15:28:19 +0200

    feat: create checkout session

--- Dshop/apps/payments/urls.py
+++ Dshop/apps/payments/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import PurchaseView, stripe_config
+from .views import PurchaseView, stripe_config, create_checkout_session
 
 urlpatterns = [
     path('', PurchaseView.as_view(), name='purchase'),
     path('config/', stripe_config),
+    path('create-checkout-session/', create_checkout_session),
 ]
