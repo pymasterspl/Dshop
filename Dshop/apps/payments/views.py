0723c8a1ca3a4bce62bc126c1e92dc5a176cf119
memorisanka <j.krawczyk87@gmail.com>
2023-10-08 16:47:45 +0200

    feat: stripe webhook implementation

--- Dshop/apps/payments/views.py
+++ Dshop/apps/payments/views.py
@@ -19,24 +19,17 @@ def stripe_config(request):
 @csrf_exempt
 def create_checkout_session(request):
     if request.method == 'GET':
-        domain_url = 'http://localhost:8000/'
+        domain_url = 'http://localhost:8000/payments/'
         stripe.api_key = settings.STRIPE_SECRET_KEY
         product_price = 99.99
         try:
-            # Create new Checkout Session for the order
-            # Other optional params include:
-            # [billing_address_collection] - to display billing address details on the page
-            # [customer] - if you have an existing Stripe Customer ID
-            # [payment_intent_data] - capture the payment later
-            # [customer_email] - prefill the email input in the form
-            # For full details see https://stripe.com/docs/api/checkout/sessions/create
-
-            # ?session_id={CHECKOUT_SESSION_ID} means the redirect will have the session ID set as a query param
             checkout_session = stripe.checkout.Session.create(
-                success_url=domain_url + 'payments/success?session_id={CHECKOUT_SESSION_ID}',
-                cancel_url=domain_url + 'payments/cancelled/',
+                client_reference_id=request.user.id if request.user.is_authenticated else None,
+                success_url=domain_url + 'success?session_id={CHECKOUT_SESSION_ID}',
+                cancel_url=domain_url + 'cancelled/',
                 payment_method_types=['card'],
                 mode='payment',
+                # TODO: implementacja produktu z bazy danych
                 line_items=[
                     {
                         'quantity': 1,
