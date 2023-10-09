f89c69e3bb9ea4e69d5e3c87db856dca48db5ef0
memorisanka <j.krawczyk87@gmail.com>
2023-10-09 13:07:31 +0200

    tests: use requests_mock to make test independent from external resource

--- Dshop/apps/payments/tests/test_checkout_session.py
+++ Dshop/apps/payments/tests/test_checkout_session.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
+from django.core.serializers.json import DjangoJSONEncoder
 from django.test import TestCase
 from django.urls import reverse
+from requests_mock import Mocker
 
 
 class PaymentTestCase(TestCase):
@@ -8,7 +10,11 @@ class PaymentTestCase(TestCase):
         self.user = User.objects.create_user(username='testuser', password='testpassword')
 
     def test_create_checkout_session(self):
-        self.client.login(username='testuser', password='testpassword')
-        response = self.client.get(reverse('create_checkout_session'))
-        self.assertEqual(response.status_code, 200)
-        self.assertIn('sessionId', response.json())
+        with Mocker(json_encoder=DjangoJSONEncoder) as mocker:
+            mocker.register_uri('POST', 'https://api.stripe.com/v1/checkout/sessions', json={'id': 'mocked_session_id'})
+
+            self.client.login(username='testuser', password='testpassword')
+            response = self.client.get(reverse('create_checkout_session'))
+
+            self.assertEqual(response.status_code, 200)
+            self.assertIn('sessionId', response.json())
