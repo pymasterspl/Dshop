01e79cfa568302343d6dce8f76a15a4ede7206fd
Marta Sommer <marta.sommer90@gmail.com>
2023-10-12 21:23:54 +0200

    static files support

--- Dshop/Dshop/urls.py
+++ Dshop/Dshop/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+import os
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('apps.products_catalogue.urls')),
+    path('', include('apps.products_catalogue.urls'))
 ]
+
+if not os.environ.get("IS_PRODUCTION"):
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
