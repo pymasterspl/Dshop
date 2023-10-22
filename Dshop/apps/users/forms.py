c40611789372e5027300afdca946909a8c512e77
Pawelooo <paweloo0147@gmail.com>
2023-10-22 10:13:32 +0200

    user-management

--- Dshop/apps/users/forms.py
+++ Dshop/apps/users/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
 from .models import User, CustomUser
 
 
