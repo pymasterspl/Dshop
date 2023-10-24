9055006317ea07b32ce5e31ea8cf9d4f2d78e727
knop-k <knop.krzysiek@gmail.com>
2023-10-24 12:02:24 +0200

    feat: configuration tinymce

--- Dshop/Dshop/settings.py
+++ Dshop/Dshop/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'sorl.thumbnail',
+    'tinymce',
 
 ] + PROJECT_APPS
 
@@ -169,3 +170,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 THUMBNAIL_PREFIX = 'cache/'
 
+TINYMCE_DEFAULT_CONFIG = {
+    "theme": "silver",
+    "width": 500,
+    "height": 300,
+    "menubar": False,
+    "plugins": "lists",
+    "toolbar": " formatselect  | bold italic | bullist "
+}
