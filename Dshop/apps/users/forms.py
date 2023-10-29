f9e8054cf5c0124d82e1bbfaf8b5d471b7039ca6
Pawelooo <paweloo0147@gmail.com>
2023-10-29 17:55:38 +0100

    update users

--- Dshop/apps/users/forms.py
+++ Dshop/apps/users/forms.py
@@ -1,6 +1,7 @@
+
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
 from .models import User, CustomUser
+from django.contrib.auth.forms import UserCreationForm
 
 
 class CustomUserForm(UserCreationForm):
