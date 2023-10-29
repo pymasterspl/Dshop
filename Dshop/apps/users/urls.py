dbc8d36ebad722a62a00c9f64320483409b6d009
Pawelooo <paweloo0147@gmail.com>
2023-10-29 18:00:11 +0100

    update users

--- Dshop/apps/users/urls.py
+++ Dshop/apps/users/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import RegistrationView, LoginUserView, LogoutView, HomeView, \
     RemoveUserView, UpdateUserView
 
+
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('registration/', RegistrationView.as_view(), name='registration'),
@@ -10,6 +11,3 @@ urlpatterns = [
     path('delete-user/', RemoveUserView.as_view(), name='delete_user'),
     path('update-user/', UpdateUserView.as_view(), name='update_user')
 ]
-
-
-
