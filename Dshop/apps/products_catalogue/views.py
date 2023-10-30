1d9dece9981bfab646aa7da9504e728c54141339
memorisanka <j.krawczyk87@gmail.com>
2023-10-30 21:30:18 +0100

    feat: add additional images in loop

--- Dshop/apps/products_catalogue/views.py
+++ Dshop/apps/products_catalogue/views.py
@@ -35,10 +35,10 @@ class CeneoProductListView(View):
 
         for product in products:
             o_element = etree.SubElement(root, 'o', id=str(product.pk), url=str(), price=str(product.price),
-                                         avail=str(),   # TODO: url and available items
+                                         avail=str(),  # TODO: url and available items
                                          weight=str(),
                                          stock=str(),
-                                         basket=str())  
+                                         basket=str())
 
             cat_element = etree.SubElement(o_element, 'cat')
             cat_element.text = etree.CDATA(product.category.name)
@@ -46,11 +46,24 @@ class CeneoProductListView(View):
             name_element = etree.SubElement(o_element, 'name')
             name_element.text = etree.CDATA(product.name)
 
+            if product.featured_photos.exists():
+                featured_image_url = product.featured_photos.first().image.url
+                print(featured_image_url)
+            else:
+                featured_image_url = ''
             imgs_element = etree.SubElement(o_element, 'imgs')
             main_element = etree.SubElement(imgs_element, 'main')
+            main_element.set('url', featured_image_url)
 
-            main_element.set('url', str(product.images))  # TODO: get correct url to featured image
             # TODO : to add additional images in loop
+            additional_images = product.images.all()
+            for i, additional_image in enumerate(additional_images, start=1):
+                additional_img_element = etree.SubElement(imgs_element, 'i', id=str(i))
+                additional_img_element.set('url', additional_image.image.url)
+
+                if i >= 20:
+                    break
+
             desc_element = etree.SubElement(o_element, 'desc')
             desc_element.text = etree.CDATA(product.full_description)
 
@@ -80,6 +93,7 @@ class CeneoCategoriesView(View):
             return response.content
         except requests.RequestException as e:
             raise CeneoAPIException(f"Failed to fetch data from Ceneo API: {e}")
+
     # TODO: to add parent category to the model and add functionality to write it to database
     def parse_categories(self, category_elem):
         categories = []
