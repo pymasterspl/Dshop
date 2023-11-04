6c6aebdc0be7d42fc61a7d93fa02af824880a956
Jan Gregorczyk <jnk.gregorczyk@gmail.com>
2023-11-04 16:41:08 +0100

    tests:Added more tests for PriceChangeHistory

--- Dshop/apps/products_catalogue/tests/test_price_change_history.py
+++ Dshop/apps/products_catalogue/tests/test_price_change_history.py
@@ -9,6 +9,15 @@
 import pytest
 from apps.products_catalogue.models import PriceChangeHistory, Product, Category
 
+
+# use https://pypi.org/project/freezegun/ for time management in tests
+# add __init__ method to product and store current price in local variable
+# in save check if price changed
+# if price changed 
+#   - update last existing PriceCHangeHistory object with disabled_at = current time
+#   - create new PriceCHangeHistory with new price
+
+
 @pytest.mark.django_db # to be deleted
 def test_model_structure():
     # This kind of test is for fun mostly and will be deleted in going further. 
@@ -28,26 +37,51 @@ def test_model_structure():
         assert model._meta.get_field(field)
 
 
-@pytest.mark.django_db
-def test_product_creation_creates_price_change_history():   
-    # Given
-    assert PriceChangeHistory.objects.count() == 0
+@pytest.fixture
+def create_product_with_cat():
     category = Category.objects.create(name='Test Category', is_active=True)
-
-    # When
-    product = Product.objects.create(
+    return Product.objects.create(
         name="first one",
         category=category,
         price=11,
         short_description="short desc",
         full_description="full_description"
     )
-    # Then
+
+
+@pytest.mark.django_db
+def test_product_creation_creates_price_change_history(create_product_with_cat):   
+    product = create_product_with_cat
     assert PriceChangeHistory.objects.count() == 1
 
-# use https://pypi.org/project/freezegun/ for time management in tests
-# add __init__ method to product and store current price in local variable
-# in save check if price changed
-# if price changed 
-#   - update last existing PriceCHangeHistory object with disabled_at = current time
-#   - create new PriceCHangeHistory with new price
+
+@pytest.mark.django_db
+def test_single_lowest_price_in_30_days(create_product_with_cat):
+    product = create_product_with_cat
+    assert product.lowest_price_in_30_days == product.price
+
+
+@pytest.mark.django_db
+def test_many_pricechangehistory_count(create_product_with_cat):
+    product = create_product_with_cat
+    #upper already saved once
+    product.price = 2
+    product.save()
+    product.price = 3
+    product.save()
+    product.price = 4
+    product.save()
+    assert PriceChangeHistory.objects.count() == 4
+
+
+@pytest.mark.django_db
+def test_many_lowest_price_in_30_days_all_now(create_product_with_cat):
+    product = create_product_with_cat
+    product.price = 10
+    product.save()
+    product.price = 100
+    product.save()
+    product.price = 0.1
+    product.save()
+    assert product.lowest_price_in_30_days == 0.1
+
