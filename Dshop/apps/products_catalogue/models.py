16bdea260a6b2f2075daabf12248031e421b0d70
knop-k <knop.krzysiek@gmail.com>
2023-11-15 11:52:51 +0100

    feat: add model for product attributes

--- Dshop/apps/products_catalogue/models.py
+++ Dshop/apps/products_catalogue/models.py
@@ -127,3 +127,9 @@ class PriceChangeHistory(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     disabled_at = models.DateTimeField(null=True, editable=False)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+
+
+class ProductAttribute(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    key = models.CharField(max_length=255)
+    value = models.CharField(max_length=255)
