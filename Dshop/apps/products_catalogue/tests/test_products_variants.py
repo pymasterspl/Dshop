4cc98952c25e05d988ee3440fdb009a05e601783
knop-k <knop.krzysiek@gmail.com>
2023-11-15 11:57:14 +0100

    test: add test for product attributes

--- Dshop/apps/products_catalogue/tests/test_products_variants.py
+++ Dshop/apps/products_catalogue/tests/test_products_variants.py
@@ -75,3 +75,34 @@ def test_lowest_price_in_30_days_for_product_variant():
         variant.save()
     with freeze_time("2023-11-04"):
         assert variant.lowest_price_in_30_days == pytest.approx(Decimal('0.05'))
+
+
+@pytest.mark.django_db
+def test_product_variant_attributes():
+    category = Category.objects.create(name='Test Category', is_active=True)
+    main_product = Product.objects.create(
+        name='main_product',
+        category=category,
+        price=10,
+        short_description='short desc',
+        full_description="full desc",
+    )
+    variant1 = Product.objects.create(
+        name="child product 1",
+        category=category,
+        price=11,
+        short_description="short desc",
+        full_description="full_description",
+        parent_product=main_product
+    )
+    ProductAttribute.objects.create(
+        product=variant1,
+        key='color',
+        value='red'
+    )
+    ProductAttribute.objects.create(
+        product=variant1,
+        key='size',
+        value='XL'
+    )
+    assert variant1.get_attributes().count() == 2
