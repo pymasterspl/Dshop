bbcec68bdc626f4468402de93121d0ee54697120
Mateusz Świetlik <matiks@gmail.com>
2023-11-21 21:23:46 +0100

    removed unused imports

--- Dshop/apps/users/api_views.py
+++ Dshop/apps/users/api_views.py
@@ -1,8 +1,6 @@
 from django.contrib.auth import get_user_model
-from django.utils.translation import gettext_lazy as _
-from rest_framework import viewsets, status, mixins
+from rest_framework import viewsets, mixins
 from rest_framework.permissions import AllowAny
-from rest_framework.response import Response
 
 from .models import CustomUser
 from .serializers import RegistrationSerializer
