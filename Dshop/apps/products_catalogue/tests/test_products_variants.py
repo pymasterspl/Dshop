5a24ffd8753fce9f8fbf762adb09a60e3894d71d
knop-k <knop.krzysiek@gmail.com>
2023-11-21 14:48:01 +0100

    test: add test for category detail view

--- Dshop/apps/products_catalogue/tests/test_products_variants.py
+++ Dshop/apps/products_catalogue/tests/test_products_variants.py
@@ -89,3 +89,53 @@ def test_product_detail_view(client):
     url = reverse("product-detail", kwargs={'slug': 'main_product', 'id': main_product.id})
     response = client.get(url)
     assert_object_product_response(response, category)
+
+
+def assert_object_category_response(response, category):
+    context = response.context
+    assert response.status_code == 200
+    products_in_category = context.get("products")
+    assert products_in_category.count() == 1
+    assert products_in_category.first().name == 'main_product_2'
+    assert products_in_category.first().price == 10
+    assert products_in_category.first().category == category
+    assert products_in_category.first().category.name == 'Test Category'
+
+
+@pytest.mark.django_db
+def test_category_detail_view(client):
+    category = Category.objects.create(name='Test Category', is_active=True)
+    Product.objects.create(
+        name='main_product_1',
+        category=category,
+        price=10,
+        short_description='short desc',
+        full_description="full desc",
+        is_active=False
+    )
+    main_product = Product.objects.create(
+        name='main_product_2',
+        category=category,
+        price=10,
+        short_description='short desc',
+        full_description="full desc",
+    )
+    Product.objects.create(
+        name="child product 1",
+        category=category,
+        price=11,
+        short_description="short desc",
+        full_description="full_description",
+        parent_product=main_product
+    )
+    Product.objects.create(
+        name="child product 2",
+        category=category,
+        price=11,
+        short_description="short desc",
+        full_description="full_description",
+        parent_product=main_product
+    )
+    url = reverse("category-detail", kwargs={'slug': 'test-category', 'id': category.id})
+    response = client.get(url)
+    assert_object_category_response(response, category)
