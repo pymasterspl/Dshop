27c296ae72d0e504fe7b2ab8b0f2d1c8237ad884
knop-k <knop.krzysiek@gmail.com>
2023-11-21 12:48:53 +0100

    refactor: remove test products variants

--- Dshop/apps/products_catalogue/tests/test_products_variants.py
+++ Dshop/apps/products_catalogue/tests/test_products_variants.py
@@ -44,34 +44,3 @@ def test_product_creation_with_variants():
     assert main_product.product_set.count() == 2
     assert child1.parent_product == main_product
     assert child2.parent_product == main_product
-
-
-@pytest.mark.django_db
-def test_product_variant_attributes():
-    category = Category.objects.create(name='Test Category', is_active=True)
-    main_product = Product.objects.create(
-        name='main_product',
-        category=category,
-        price=10,
-        short_description='short desc',
-        full_description="full desc",
-    )
-    variant1 = Product.objects.create(
-        name="child product 1",
-        category=category,
-        price=11,
-        short_description="short desc",
-        full_description="full_description",
-        parent_product=main_product
-    )
-    ProductAttribute.objects.create(
-        product=variant1,
-        key='color',
-        value='red'
-    )
-    ProductAttribute.objects.create(
-        product=variant1,
-        key='size',
-        value='XL'
-    )
-    assert variant1.get_attributes().count() == 2
