f5da487c1cc49863e8a5979373ad7fbc92694a30
knop-k <knop.krzysiek@gmail.com>
2023-12-18 16:17:57 +0100

    feat: create serializers for product

---
+++ Dshop/apps/products_catalogue/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import Product
+
+
+class ProductSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Product
+        fields = ['id', 'category', 'name', 'price', 'short_description', 'full_description', 'parent_product']
