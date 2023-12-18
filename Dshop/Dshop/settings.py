4f06486a5e690c36f09673b28ede90b7275cf6e8
knop-k <knop.krzysiek@gmail.com>
2023-12-18 16:17:01 +0100

    feat: settings for pagination and filters

--- Dshop/Dshop/settings.py
+++ Dshop/Dshop/settings.py
@@ -208,6 +208,10 @@ REST_FRAMEWORK = {
     },
     "TEST_REQUEST_DEFAULT_FORMAT": "json",
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    "PAGE_SIZE": 25,
+    "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"]
+
 }
 
 # drf_spectacular
