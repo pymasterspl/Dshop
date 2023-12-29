b0ecc7bc8123fb729d614885e654ba1039811de7
Krzysztof Knop <knop.krzysiek@gmail.com>
2023-12-29 13:01:07 +0100

    feat: add is_active in filters

--- Dshop/apps/products_catalogue/filters.py
+++ Dshop/apps/products_catalogue/filters.py
@@ -4,7 +4,8 @@ from .models import Product
 
 class ProductFilter(filters.FilterSet):
     name = filters.CharFilter(field_name='name', lookup_expr='icontains')
+    is_active = filters.BooleanFilter(field_name='is_active')
 
     class Meta:
         model = Product
-        fields = ['name']
+        fields = ['name', 'is_active']
