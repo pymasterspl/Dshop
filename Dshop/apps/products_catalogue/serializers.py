e90c06768e133dd1978170c07529613cff0f1666
Krzysztof Knop <knop.krzysiek@gmail.com>
2023-12-29 12:10:02 +0100

    feat: add read only fields for 'id' in serializers

--- Dshop/apps/products_catalogue/serializers.py
+++ Dshop/apps/products_catalogue/serializers.py
@@ -7,3 +7,4 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ['id', 'category', 'name', 'price', 'short_description', 'full_description', 'parent_product']
+        read_only_fields = ['id']
