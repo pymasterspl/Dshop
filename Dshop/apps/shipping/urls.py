e251bdca439fea4572c6d3fc117ed22ec0865ead
memorisanka <j.krawczyk87@gmail.com>
2024-01-02 21:22:05 +0100

    feat: add urls

--- Dshop/apps/shipping/urls.py
+++ Dshop/apps/shipping/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import InPostSendingMethodsView
+from .views import InPostSendingMethodsView, PaczkomatPageView, HandleMethodChoiceView
 
 urlpatterns = [
     path('', InPostSendingMethodsView.as_view(), name='shipping_methods'),
+    path('paczkomat/', PaczkomatPageView.as_view(), name='paczkomat_page'),
+    path('shipping-method/', HandleMethodChoiceView.as_view(), name='handle_method_choice')
 ]
