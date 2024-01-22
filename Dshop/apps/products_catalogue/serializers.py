aba3bd0241a0db4e0834d925ee9137539b0b76f7
Jacek Ostański <jacek@ivolution.pl>
2024-01-22 11:14:25 +0100

    Debug and fix serializers

--- Dshop/apps/products_catalogue/serializers.py
+++ Dshop/apps/products_catalogue/serializers.py
@@ -42,31 +42,48 @@ example output
     }
 
 """
-    
+
 
 class CartItemSerializer(serializers.Serializer):
     product_pk = serializers.IntegerField(min_value=1)
     quantity = serializers.IntegerField(min_value=1)
     product_name = serializers.CharField(read_only=True, max_length=200)
-    price = serializers.DecimalField(read_only=True, min_value=0, max_digits=10, decimal_places=2)
-    subtotal = serializers.DecimalField(read_only=True, min_value=0, max_digits=10, decimal_places=2)
+    price = serializers.DecimalField(
+        read_only=True, min_value=0, max_digits=10, decimal_places=2
+    )
+    subtotal = serializers.DecimalField(
+        read_only=True, min_value=0, max_digits=10, decimal_places=2
+    )
     item_id = serializers.IntegerField(read_only=True, min_value=1)
+
+    # zapewne będziesz tutaj musial napisac wlasne: to_internal_value albo lepiej create i update
+    # https://www.django-rest-framework.org/api-guide/serializers/#baseserializer
+    # aczkolwiek może warto będzie zrobić create i update w CartSerializer, zamiast tutaj.
+    # Dlaczego? Bo tam masz bez problemu dostęp do pełnego cart.
+    # Niemniej, zachowaj ten serializer do walidacji danych.
 
 
 class CartSerializer(serializers.Serializer):
-    total = serializers.DecimalField(read_only=True, min_value=0, max_digits=10, decimal_places=2)
-    items = CartItemSerializer(many=True)
+    total = serializers.DecimalField(
+        read_only=True, min_value=0, max_digits=10, decimal_places=2
+    )
+    items = CartItemSerializer(many=True, write_only=True)
+    items = serializers.SerializerMethodField()
     count = serializers.IntegerField(read_only=True, min_value=1)
 
+    def get_items(self, obj):
+        print("alamakota get_items")
+        print(obj)
+        return "ala"  # co tu zwrócisz będzie w items
+        # dokumentacja: https://www.django-rest-framework.org/api-guide/fields/#serializermethodfield
 
     def validate_items(self, items):
         item_ids = set()
         for item in items:
-            item_id = item.get('item_id')
+            item_id = item.get("item_id")
             if item_id in item_ids:
-                raise serializers.ValidationError("item_id must be unique within items.")
+                raise serializers.ValidationError(
+                    "item_id must be unique within items."
+                )
             item_ids.add(item_id)
         return items
-
-    # def to_internal_value(self, data):
-    #     return data
