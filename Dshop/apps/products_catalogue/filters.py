c002dfa0fdedd064f33b5c2968d28d87c2f38ac4
Jan Gregorczyk <jnk.gregorczyk@gmail.com>
2024-02-18 15:54:01 +0100

    ordering by date, availability filters added

--- Dshop/apps/products_catalogue/filters.py
+++ Dshop/apps/products_catalogue/filters.py
@@ -13,20 +13,34 @@ class ProductFilter(filters.FilterSet):
     category_name = filters.ChoiceFilter(field_name='category__name',
                                          choices=Category.objects.all().values_list('name', 'name').distinct(),
                                          label='Category', empty_label='Wybierz Kategorie')
+    availability = filters.ChoiceFilter(
+                                        choices=Product.AVAILABILITY_CHOICES,
+                                        label="Dostępność",
+                                        empty_label="Wszystkie",
+                                        method='filter_availability')
     order_by = filters.OrderingFilter(
         fields=(
             ('price', 'price'),
             ('name', 'name'),
+            ('created_at', 'created_at')
         ),
         field_labels={
             'price': 'Cena rosnąco',
             '-price': 'Cena malejąco',
             '-name': 'Produkt A-Z',
             'name': 'Produkt Z-A',
+            'created_at': 'Najnowsze',
+            '-created_at': 'Najstarsze',
         },
         label='Sortuj',
         empty_label='Domyślnie'
     )
+
+    def filter_availability(self, queryset, name, value):
+        value = int(value)
+        if value in (1, 3, 7, 14):
+            return queryset.filter(availability__lte=value)
+        return queryset.filter(**{name: value})
 
     def __init__(self, *args, **kwargs):
         super(ProductFilter, self).__init__(*args, **kwargs)
@@ -38,4 +52,4 @@ class ProductFilter(filters.FilterSet):
 
     class Meta:
         model = Product
-        fields = ['name', 'price', 'category_name']
+        fields = ['name', 'price', 'category_name', 'availability']
