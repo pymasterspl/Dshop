a511cf62d2c221a37546245f03d912120c3c191f
Jacek Ostański <jacek@ivolution.pl>
2024-02-19 18:54:32 +0100

    example tests

--- Dshop/apps/products_catalogue/tests/conftest.py
+++ Dshop/apps/products_catalogue/tests/conftest.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 import pytest
 from rest_framework.test import APIClient
+from apps.products_catalogue.models import Category, Product
 
 
 @pytest.fixture
@@ -9,3 +10,18 @@ def api_client():
     user = User.objects.create_user(username='testuser', password='testpassword')
     client.force_authenticate(user)
     return client
+
+
+@pytest.fixture
+def products():
+    category = Category.objects.create(name='Test Category', is_active=True)
+    for price in range(1, 11):
+        Product.objects.create(
+            name=f"main product ${price}",
+            category=category,
+            price=price,
+            short_description="short desc inactive",
+            full_description="full_description inactive",
+            is_active=True
+        )
+
---
+++ Dshop/apps/products_catalogue/tests/test_products_filters.py
@@ -0,0 +1,21 @@
+import pytest
+
+
+from django.urls import reverse
+
+from apps.products_catalogue.models import Category, Product
+
+
+@pytest.mark.django_db
+def test_filter_by_price(client, products):
+    # Products is a fixture with 9 products and diff prices. 
+    # checking ordering
+    # Given is in fixture
+    # When
+    url = reverse("products-list")
+    response = client.get(f"{url}?order_by=-price")
+    products = response.context["object_list"]
+    # Then
+    for idx, price in enumerate(range (10, 1, -1)):
+        assert products[idx].price == price
+
