0ec8867b1e501fb6b8736deb241e15d2a0997651
Jan Gregorczyk <jnk.gregorczyk@gmail.com>
2024-03-09 16:46:46 +0100

    fix: deleted unused api_permissions

--- Dshop/apps/products_catalogue/api_permissions.py
+++
@@ -1,5 +0,0 @@
-from rest_framework.permissions import BasePermission, SAFE_METHODS
-
-class IsStaffOrReadOnly(BasePermission):
-    def has_permission(self, request, view):
-        return (request.method in SAFE_METHODS) or request.user.is_staff == True
